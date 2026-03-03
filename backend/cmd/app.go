b889ac7ebeef6241841bbdbafff4328b89d2d502
cajr11 <caondo@yahoo.co.uk>
2026-03-03 08:02:10 -0600

    feat: initial server setup

--- backend/cmd/app.go
+++ backend/cmd/app.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
 func main(){
-  println("Hello world")
+  r := chi.NewRouter()
+  r.Get("/", serve)
+  http.ListenAndServe(":8080", r)
+}
+
+
+func serve(w http.ResponseWriter, r *http.Request){
+   println("server running on port 8080")
+   w.Write([]byte("welcome"))
 }
