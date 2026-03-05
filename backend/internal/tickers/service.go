a5e01280050c541288852e51d5412d2339151ce4
cajr11 <caondo@yahoo.co.uk>
2026-03-05 15:36:01 -0600

    feat(tickers): add pair fetching service

---
+++ backend/internal/tickers/service.go
@@ -0,0 +1,40 @@
+package tickers
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+/**
+ Function for getting all tokens against USDT. Fetches all tokens then filters for USDT pairs.
+**/
+func  getAllPairs() ([]BinanceSymbol, error){
+  url := "https://api.binance.com/api/v3/exchangeInfo?permissions=SPOT&showPermissionSets=false"
+
+  res, err := http.Get(url)
+
+  if err != nil {
+	return nil, err
+  }
+
+  body, err := io.ReadAll(res.Body)
+
+  if err != nil {
+	return nil, err
+  }
+
+  var data BinanceExchangeResult
+
+  err = json.Unmarshal([]byte(body), &data)
+
+  if err != nil {
+  return nil, err
+  }
+
+  data.FilterUSDTPairs()
+
+  return data.Symbols, nil
+
+  
+}
