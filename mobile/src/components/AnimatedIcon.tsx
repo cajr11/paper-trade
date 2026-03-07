0f0fe795fbc91b3104dd44e1ee6c394b85c687d6
cajr11 <caondo@yahoo.co.uk>
2026-03-07 08:32:57 -0600

    feat(nav): add all app nav bar tabs and icons

---
+++ mobile/src/components/AnimatedIcon.tsx
@@ -0,0 +1,150 @@
+import { Image } from "expo-image";
+import { useState } from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+import Animated, { Easing, Keyframe } from "react-native-reanimated";
+import { scheduleOnRN } from "react-native-worklets";
+
+const INITIAL_SCALE_FACTOR = Dimensions.get("screen").height / 90;
+const DURATION = 5000;
+
+export function AnimatedSplashOverlay() {
+  const [visible, setVisible] = useState(true);
+
+  if (!visible) return null;
+
+  const splashKeyframe = new Keyframe({
+    0: {
+      opacity: 1,
+    },
+    20: {
+      opacity: 1,
+    },
+    70: {
+      opacity: 0,
+      easing: Easing.elastic(0.7),
+    },
+    100: {
+      opacity: 0,
+      easing: Easing.elastic(0.7),
+    },
+  });
+
+  return (
+    <Animated.View
+      entering={splashKeyframe.duration(DURATION).withCallback((finished) => {
+        "worklet";
+        if (finished) {
+          scheduleOnRN(setVisible, false);
+        }
+      })}
+      style={styles.splashBg}
+    >
+      <Animated.View style={styles.splashContainer}>
+        <Animated.Image
+          style={{
+            width: 300,
+            height: 300,
+            resizeMode: "contain",
+          }}
+          source={require("@/assets/images/paper-trade-logo-light.png")}
+        />
+      </Animated.View>
+    </Animated.View>
+  );
+}
+
+const keyframe = new Keyframe({
+  0: {
+    transform: [{ scale: INITIAL_SCALE_FACTOR }],
+  },
+  100: {
+    transform: [{ scale: 1 }],
+    easing: Easing.elastic(0.7),
+  },
+});
+
+const logoKeyframe = new Keyframe({
+  0: {
+    transform: [{ scale: 1.3 }],
+    opacity: 0,
+  },
+  40: {
+    transform: [{ scale: 1.3 }],
+    opacity: 0,
+    easing: Easing.elastic(0.7),
+  },
+  100: {
+    opacity: 1,
+    transform: [{ scale: 1 }],
+    easing: Easing.elastic(0.7),
+  },
+});
+
+export function AnimatedIcon() {
+  return (
+    <View style={styles.iconContainer}>
+      <Animated.View
+        entering={keyframe.duration(DURATION)}
+        style={styles.background}
+      />
+      <Animated.View
+        style={styles.imageContainer}
+        entering={logoKeyframe.duration(DURATION)}
+      >
+        <Image
+          style={styles.image}
+          source={require("@/assets/images/paper-trade-logo.png")}
+        />
+      </Animated.View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    flex: 1,
+  },
+  glow: {
+    width: 201,
+    height: 201,
+    position: "absolute",
+  },
+  iconContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    flex: 1,
+    width: "100%",
+    height: 128,
+    zIndex: 100,
+  },
+  image: {
+    position: "absolute",
+    width: 76,
+    height: 71,
+  },
+  background: {
+    borderRadius: 40,
+    experimental_backgroundImage: `linear-gradient(180deg, #3C9FFE, #0274DF)`,
+    width: 128,
+    height: 128,
+    position: "absolute",
+  },
+  splashBg: {
+    ...StyleSheet.absoluteFill,
+    backgroundColor: "#000",
+    zIndex: 1000,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  splashContainer: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 16,
+  },
+});
---
+++ mobile/src/components/AppTabs.tsx
@@ -0,0 +1,69 @@
+import { NativeTabs } from "expo-router/unstable-native-tabs";
+import React from "react";
+import { useColorScheme } from "react-native";
+
+import { Colors } from "@/constants/theme";
+
+export default function AppTabs() {
+  const scheme = useColorScheme();
+  const colors = Colors[scheme === "unspecified" ? "light" : scheme];
+
+  return (
+    <NativeTabs
+      backgroundColor={colors.background}
+      indicatorColor={colors.backgroundElement}
+      labelStyle={{ selected: { color: colors.text } }}
+    >
+      <NativeTabs.Trigger name="index">
+        <NativeTabs.Trigger.Label>Dashboard</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon
+          sf={{
+            default: "chart.pie",
+            selected: "chart.pie.fill",
+          }}
+          renderingMode="template"
+        />
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="explore">
+        <NativeTabs.Trigger.Label>Explore</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon
+          sf={{
+            default: "sparkle.magnifyingglass",
+            selected: "sparkle.magnifyingglass",
+          }}
+        />
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="watchlist">
+        <NativeTabs.Trigger.Label>Watchlist</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon
+          sf={{
+            default: "star",
+            selected: "star.fill",
+          }}
+        />
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="history">
+        <NativeTabs.Trigger.Label>History</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon
+          sf={{
+            default: "clock.arrow.trianglehead.counterclockwise.rotate.90",
+            selected: "clock.arrow.trianglehead.counterclockwise.rotate.90",
+          }}
+        />
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="profile">
+        <NativeTabs.Trigger.Label>Person</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon
+          sf={{
+            default: "person",
+            selected: "person.fill",
+          }}
+        />
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
+}
