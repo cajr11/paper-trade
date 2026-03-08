65c4ff43e07df7c0ec18c76efa246190f663c70d
cajr11 <caondo@yahoo.co.uk>
2026-03-08 10:46:34 -0600

    feat(ui): add LogoImage to login screen

--- mobile/src/app/login.tsx
+++ mobile/src/app/login.tsx
@@ -1,18 +1,71 @@
 import React from "react";
 import { ThemedView } from "@/components/ui/ThemedView";
 import { ThemedText } from "@/components/ui/ThemedText";
+import { Image } from "expo-image";
+import { ColorSchemeName, TextInput, useColorScheme } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Colors } from "@/constants/theme";
+
+const LogoImage = ({ scheme }: { scheme: ColorSchemeName }) => {
+  if (scheme !== "dark") {
+    return (
+      <ThemedView>
+        <Image
+          source={require("@/assets/images/paper-trade-logo.png")}
+          style={{ width: 180, height: 80 }}
+          contentFit="contain"
+        />
+      </ThemedView>
+    );
+  }
+
+  return (
+    <ThemedView>
+      <Image
+        source={require("@/assets/images/paper-trade-logo-light.png")}
+        style={{ width: 180, height: 80 }}
+        contentFit="contain"
+      />
+    </ThemedView>
+  );
+};
 
 export default function Login() {
+  const scheme = useColorScheme();
   return (
-    <ThemedView
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        borderWidth: 1,
-        padding: 16,
-      }}
-    >
-      <ThemedText>Hey</ThemedText>
-    </ThemedView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ThemedView
+        style={{
+          borderWidth: 1,
+          paddingHorizontal: 16,
+        }}
+      >
+        <LogoImage scheme={scheme} />
+        <ThemedText
+          style={{ width: 160 }}
+          type="small"
+          themeColor="textSecondary"
+        >
+          Practice crypto trading. Zero risk. Real prices.
+        </ThemedText>
+
+        <ThemedView style={{ marginTop: 50 }}>
+          <ThemedText type="smallBold" themeColor="textSecondary">
+            Email
+          </ThemedText>
+          <TextInput
+            style={{
+              height: 20,
+              width: "100%",
+              padding: 16,
+              backgroundColor:
+                Colors[(scheme as keyof typeof Colors) ?? "light"]
+                  .backgroundElement,
+            }}
+            placeholder="Enter your email"
+          />
+        </ThemedView>
+      </ThemedView>
+    </SafeAreaView>
   );
 }
