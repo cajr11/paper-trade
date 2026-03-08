4f8623c058cfe0ce07b0f485a5c00825cb4c499b
cajr11 <caondo@yahoo.co.uk>
2026-03-08 10:45:36 -0600

    chore(cleanup): move components to correct directories and correct naming

--- mobile/src/components/screens/LoadingScreen.tsx
+++ mobile/src/components/screens/LoadingScreen.tsx
@@ -1,4 +1,4 @@
-import { ThemedView } from "./ui/ThemedView";
+import { ThemedView } from "../ui/ThemedView";
 import { ActivityIndicator } from "react-native";
 
 export default function LoadingScreen() {
