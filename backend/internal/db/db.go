a4d3e5913014bae6e27adec07d33b14c9509f53d
cajr11 <caondo@yahoo.co.uk>
2026-03-10 19:04:45 -0600

    feat: create db instantiation method

---
+++ backend/internal/db/db.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+
+	"time"
+)
+
+
+func New(addr string, maxOpenConns, maxIdleConns int, maxIdleTime string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", addr)
+
+	if err != nil {
+		return nil, err
+	}
+
+
+	duration, err := time.ParseDuration(maxIdleTime)
+
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetMaxIdleConns(maxIdleConns)
+	db.SetConnMaxIdleTime(duration)
+
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	if err = db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	fmt.Println("db running")
+
+	return db, nil
+} 
